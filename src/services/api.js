d426b3aad193751dcdfbafc58d731d8ed8c774ef
Yruam Käffer de Faria <yruamkf@gmail.com>
2023-04-26 19:06:55 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from "axios"
 
 export const api = axios.create({
-    baseURL: 'https://foodexplorer-api182.herokuapp.com'
+    baseURL: 'https://foodexplorer-api-p2yq.onrender.com'
 })
